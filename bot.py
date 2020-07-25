018d8e0d76efd2c21218f0b360f2e82aaf7d363e
Matheus Lança <matheus.7910hotmail.com>
2020-07-25 16:45:18 -0300

    Adicionando implementação do BOT

---
+++ bot.py
@@ -0,0 +1,83 @@
+import tweepy
+import time
+
+from tweepy.auth import OAuthHandler
+
+def Main():
+    print("Cigarette Spaghetti BOT started")
+
+    CONSUMER_KEY = 'uXGelTxTJV5WcmabvQS5v7ByW'
+    CONSUMER_SECRET = 'Ix0iK19T7azIMclvkjCWkCeV7XbhjRPJCOC9V3VNuSSuqeuZkf'
+    ACCESS_KEY = '1285720255994777600-3cVYZT2Guecj34HKqn7eIHfXhVjXWH'
+    ACCESS_SECRET = 'ySaJFFRmFn5vpmjlbXAt967VlvcO4g6XgenosteyR5KUw'
+
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+
+    api = tweepy.API(auth, wait_on_rate_limit=True)
+
+    query = 'cigarro OR audiovisual'
+    max_tweets = 100
+
+    results = __search_for_tweets(api,query,max_tweets)
+
+    if results != None and len(results) > 0:
+        for result in results:
+            print("ID::::: ", result.id)
+            print("TEXT::::: " + result.text)
+            print("USER::::: ", result.user.name)
+            time.sleep(2)
+            __fav_tweet(api,result.id)
+            __retweet(api, result.id)
+
+            firstMessage = 'Olá, meus queridxs. Venho através deste bot, para apresentar pra vocês. Este curta experimental colaborativo que dirigimos e produzimos em meio a quarentena.'
+            secondMessage = ' Espero que gostem e se possível, compartilhem. Vamos fortalecer o cenário do audiovisual independente e a cultura.\n\n https://youtu.be/qcRar3JnNOI'
+
+            firstReply = __reply_tweet(api,firstMessage,result.id)
+
+            if firstReply != None:
+                secondReply = __reply_tweet(api,secondMessage,firstReply.id)
+
+            print("Flow completed successfully")
+            time.sleep(5)
+
+    else:
+        print("There is no tweets to reply")
+
+def __search_for_tweets(api,query,max_tweets):
+    try:
+        print("trying to search tweets")
+        return api.search(q=query, count=max_tweets,result_type="recent",locale="pt-br")
+    except TweepError as err:
+        print(err)
+        print("Could not search for new tweets")
+
+def __fav_tweet(api,tweetId):
+    try:
+        print("trying to fav tweet")
+        api.create_favorite(tweetId)
+    except TweepError as err:
+        print(err)
+        print("Could not fav this tweet")
+
+
+def __reply_tweet(api,reply_message, id):
+    try:
+        print("trying to reply tweet")
+        return api.update_status(reply_message, in_reply_to_status_id=id, auto_populate_reply_metadata=True)
+    except TweepError as err:
+        print(err)
+        print("Could not reply this tweet")
+
+def __retweet(api,id):
+    try:
+        print("trying to rt tweet")
+        api.retweet(id)
+    except TweepError as err:
+        print(err)
+        print("Could not retweet this tweet")
+
+while True:
+    Main()
+    time.sleep(600)
